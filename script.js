8b6b4ce361d0b0caa02097c5cc51f04f4041aa42
webfullsympathy <Aso.Taisei@outlook.jp>
2024-08-28 11:37:46 +0900

    ミス直し

--- script.js
+++
@@ -1,7 +0,0 @@
-window.onload = function(){
-    document.body.addEventListener('right-click-menu',function(e){
-        document.getElementById('right-click-menu').style.left=e.pageX+"px";
-        document.getElementById('right-click-menu').style.top=e.pageY+"px";
-        document.getElementById('right-click-menu').style.display="block";
-    });
-}
